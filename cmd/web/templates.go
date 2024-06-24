4511a18766932fa1c69f0df6a6099893fb4f8d9b
Abdugaffar Kutbiddinov <a.kutbiddinov@allerhand.ru>
2024-06-24 14:20:04 +0300

    cross-site request forgery attack prevention functionality added

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -22,6 +22,7 @@ type templateData struct {
 	Form            any
 	Flash           string
 	IsAuthenticated bool
+	CSRFToken       string
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
