d2d97f5a0f7f952e540ec43de0bad08423900fc9
Abdugaffar Kutbiddinov <a.kutbiddinov@allerhand.ru>
2024-07-02 16:47:34 +0300

    mock tests added

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -18,3 +18,61 @@ func TestPing(t *testing.T) {
 	assert.Equal(t, code, http.StatusOK)
 	assert.Equal(t, body, "OK")
 }
+
+func TestSnippetView(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+
+	defer ts.Close()
+
+	tests := []struct {
+		name     string
+		url      string
+		wantCode int
+		wantBody string
+	}{
+		{
+			name:     "Valid ID",
+			url:      "/snippet/view/1",
+			wantCode: http.StatusOK,
+			wantBody: "An old silent pond",
+		},
+		{
+			name:     "Non-existent ID",
+			url:      "/snippet/view/2",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "Negative ID",
+			url:      "/snippet/view/-1",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "Decimal ID",
+			url:      "/snippet/view/1.23",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "String ID",
+			url:      "/snippet/view/foo",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "Empty ID",
+			url:      "/snippet/view/",
+			wantCode: http.StatusNotFound,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			code, _, body := ts.get(t, tt.url)
+			assert.Equal(t, code, tt.wantCode)
+
+			if tt.wantBody != "" {
+				assert.StringContains(t, body, tt.wantBody)
+			}
+		})
+	}
+}
